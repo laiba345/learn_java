e8bc9627e961d2747289bf80350cc22b8bf08ffb
Kang Huang <kangder@KangdeMBP.lan>
2023-05-20 16:45:45 +0800

    HelloWorld

---
+++ JavaSECode/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: HelloWorld
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Null_jun
+ * @Create 2023/5/20 16:44
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
